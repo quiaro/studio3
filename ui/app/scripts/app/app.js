2623cb64245f2cd049abc0573a9f94740b1019f5
quiaro <david.quiros@rivetlogic.com>
2013-08-22 18:43:56 -0600

    Style studio UI navigation and update bootstrap to version 3.0

--- ui/app/scripts/app/app.js
+++ ui/app/scripts/app/app.js
@@ -19,7 +19,7 @@ angular.module('s2doApp', [
         controller: 'AppCtrl'
       })
       .otherwise({
-        redirectTo: '/preview'
+        redirectTo: '/dashboard'
       });
   })
 
--- ui/app/scripts/common/directives/navigation.js
+++ ui/app/scripts/common/directives/navigation.js
@@ -7,6 +7,9 @@ angular.module('directives.navigation', [])
 			restrict: 'E',
 			replace: true,
 			template: '<div class="navigation">' +
+								'  <div class="logo">' +
+								'    <a href="#"><img src="/img/crafter-logo.png" alt="" /></a>' +
+								'  </div>' +
 								'  <search></search>' +
 								'  <fileview></fileview>' +
 								'  <iconmenu></iconmenu>' +
@@ -19,8 +22,14 @@ angular.module('directives.navigation', [])
 			restrict: 'E',
 			replace: true,
 			template: '<div class="search">' +
-								'  <input type="text" placeholder="search..." />' +
-								'  <button type="button" class="btn btn-primary" ng-click="search()" />' +
+							  '  <form class="form-inline" role="form">' +
+							  '    <div class="form-group">' +
+								'      <input type="search" class="form-control input-sm" placeholder="search..." />' +
+								'      <button type="button" class="btn btn-primary btn-sm" ng-click="search()">' +
+								'        <span class="glyphicon glyphicon-search"></span>' +
+								'      </button>' +
+								'    </div>' +
+							  '  </form>' +
 								'</div>'
 		};
 	})
@@ -37,10 +46,16 @@ angular.module('directives.navigation', [])
 		return {
 			restrict: 'E',
 			replace: true,
-			template: '<ul class="icon-menu">' +
-								'  <li><a class="icon-item" href="#">John Doe</a></li>' +
-								'  <li class="separator"></li>' +
-								'  <li><a class="icon-item" href="#">John Doe</a></li>' +
+			template: '<ul class="icon-menu list-inline pull-right">' +
+								'  <li><a href="#">' +
+								'     <span class="glyphicon glyphicon-user"></span>' +
+								'			<span>Scott Weiland</span>' +
+								'	 </a></li>' +
+								'  <li class="separator">|</li>' +
+								'  <li><a href="#">' +
+								'     <span class="glyphicon glyphicon-log-out"></span>' +
+								'			<span>Sign Out</span>' +
+								'	 </a></li>' +
 								'</ul>'
 		};
 	});
