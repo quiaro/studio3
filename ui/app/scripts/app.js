648355b9bde9d7da8457aa23f5a197019b9f7db4
quiaro <david.quiros@rivetlogic.com>
2013-09-26 14:30:58 -0600

    Add project and user menus to navigation

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('studio-ui', [
+        'navigation',
         'dashboard',
         'preview',
         'common',
--- ui/app/scripts/common/directives/navigation.js
+++ ui/app/scripts/common/directives/navigation.js
@@ -6,20 +6,8 @@ angular.module('common')
 		return {
 			restrict: 'E',
 			replace: true,
-			template: '<div class="navigation">' +
-								'  <div class="logo"></div>' +
-								'  <search></search>' +
-								'  <fileview></fileview>' +
-								'  <iconmenu></iconmenu>' +
-								'  <div class="language">' +
-								'    <input type="button" value="English" ' +
-								'       ng-click="AppCtrl.changeLanguage(\'en\')" />' +
-								'    <input type="button" value="French" ' +
-								'       ng-click="AppCtrl.changeLanguage(\'fr\')" />' +
-                                '    <input type="button" value="UV" ' +
-                                '       ng-click="AppCtrl.toggleView()" />' +
-								'  </div>' +
-								'</div>'
+			templateUrl: '/templates/navigation.tpl.html',
+            controller: 'NavigationCtrl'
 		};
 	})
 
@@ -30,13 +18,13 @@ angular.module('common')
 			template: '<div class="search">' +
                 '  <form class="form-inline" role="form">' +
                 '    <div class="form-group">' +
-								'      <input type="search" class="form-control input-sm" placeholder="search..." />' +
-								'      <button type="button" class="btn btn-primary btn-sm" ng-click="search()">' +
-								'        <span class="glyphicon glyphicon-search"></span>' +
-								'      </button>' +
-								'    </div>' +
-								'  </form>' +
-								'</div>'
+				'      <input type="search" class="form-control input-sm" placeholder="search..." />' +
+				'      <button type="button" class="btn btn-primary btn-sm" ng-click="search()">' +
+				'        <span class="glyphicon glyphicon-search"></span>' +
+				'      </button>' +
+				'    </div>' +
+				'  </form>' +
+				'</div>'
 		};
 	})
 
@@ -47,21 +35,3 @@ angular.module('common')
 			template: '<div class="file-view"></div>'
 		};
 	})
-
-	.directive('iconmenu', function () {
-		return {
-			restrict: 'E',
-			replace: true,
-			template: '<ul class="icon-menu list-inline pull-left">' +
-								'  <li><a href="#">' +
-								'     <span class="glyphicon glyphicon-user"></span>' +
-								'			<span class="description">Scott Weiland</span>' +
-								'  </a></li>' +
-								'  <li class="separator">|</li>' +
-								'  <li><a href="#">' +
-								'     <span class="glyphicon glyphicon-log-out"></span>' +
-								'			<span class="description">Sign Out</span>' +
-								'  </a></li>' +
-								'</ul>'
-		};
-	});
