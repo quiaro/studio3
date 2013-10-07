1a0bb637526540d63295aeb36daf8f7f6db81fa2
quiaro <david.quiros@rivetlogic.com>
2013-10-07 00:41:12 -0600

    Adds sample widget to use as an example

---
+++ ui/app/scripts/dashboard/plugins/sample-widget.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function () {
+
+    return {
+
+        class: "sample-widget",
+
+        header: "It's Friday, yay!",
+
+        body: "Let's have some fun!!"
+
+    };
+    
+}) ();
