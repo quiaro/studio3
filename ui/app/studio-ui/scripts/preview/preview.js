c5b54026df12e2b76fa221d76720790890349e83
quiaro <david.quiros@rivetlogic.com>
2013-10-30 18:52:20 -0600

    Adds treeview to component navigator in authoring page

--- ui/app/studio-ui/scripts/preview/preview.js
+++ ui/app/studio-ui/scripts/preview/preview.js
@@ -51,6 +51,45 @@ angular.module('preview', ['common', 'ngEventBridge'])
             });
         });
 
+        $scope.navigatorModel = [{
+                "roleName": "User",
+                "roleId": "role1",
+                "children": [{
+                    "roleName": "subUser1",
+                    "roleId": "role11",
+                    "children": []
+                }, {
+                    "roleName": "subUser2",
+                    "roleId": "role12",
+                    "children": [{
+                        "roleName": "subUser2-1",
+                        "roleId": "role121",
+                        "children": [{
+                            "roleName": "subUser2-1-1",
+                            "roleId": "role1211",
+                            "children": []
+                        }, {
+                            "roleName": "subUser2-1-2",
+                            "roleId": "role1212",
+                            "children": []
+                        }]
+                    }]
+                }]
+            },
+
+            {
+                "roleName": "Admin",
+                "roleId": "role2",
+                "children": []
+            },
+
+            {
+                "roleName": "Guest",
+                "roleId": "role3",
+                "children": []
+            }
+        ];
+
 	}]);
 
 
