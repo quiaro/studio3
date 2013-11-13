543bab61d7c225cc8ba2703b147f0608252763ec
quiaro <david.quiros@rivetlogic.com>
2013-11-13 16:04:06 -0600

    Adds login recover state (includes password recovery modal)

--- ui/client/studio-ui/src/app/login/main.js
+++ ui/client/studio-ui/src/app/login/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module('crafter.studio.login', ['ui.router'])
+angular.module('crafter.studio.login',
+    ['crafter.studio.common', 'ui.router', 'ui.bootstrap'])
 
     .constant('CONFIG', {
         baseUrl: '/studio-ui/src/app/login/'
@@ -15,9 +16,36 @@ angular.module('crafter.studio.login', ['ui.router'])
                 url: '/login',
                 templateUrl: CONFIG.baseUrl + 'templates/login.tpl.html',
                 controller: 'LoginCtrl'
+            })
+            .state('login.recover', {
+                url: '/recover',
+                onEnter: function($stateParams, $state, $modal) {
+                    $modal.open({
+                        templateUrl: CONFIG.baseUrl + 'templates/recover.tpl.html',
+                        controller: 'ModalCtrl'
+                    }).result
+                        .then(function (result) {
+                            if (result) {
+                                // If something is returned, then process it
+                                return $state.transitionTo('login');
+                            } else {
+                                // User cancelled
+                                return $state.transitionTo('login');
+                            }
+                        });
+                }
             });
     }])
 
     .controller('LoginCtrl', ['$scope', function ($scope) {
 
+    }])
+
+    .controller('ModalCtrl', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+        $scope.cancel = function () {
+            $modalInstance.close();
+        };
+        $scope.reset = function() {
+            $modalInstance.close('Some data');
+        }
     }]);
