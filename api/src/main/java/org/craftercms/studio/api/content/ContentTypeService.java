384b3ded756d346cfdf0070c53db5d988ecd737e
Sumer Jabri <sumer.jabri@craftersoftware.com>
2014-01-31 17:06:18 -0500

    API refactoring.

--- api/src/main/java/org/craftercms/studio/api/content/ContentTypeService.java
+++ api/src/main/java/org/craftercms/studio/api/content/ContentTypeService.java
@@ -24,33 +24,36 @@ public interface ContentTypeService {
      * @param site              the site to use
      * @param typeName          name of content type
      * @param formId            the form associated with this content type, see {@link org.craftercms.studio.api
-     * .content.FormService}
+     *                          .content.FormService}
      * @param defaultTemplateId default view template to use with this content type, can be null,
      *                          see {@link org.craftercms.studio.api.content.TemplateService}
      * @param templateIds       map of templates to use based on various criteria (like user-agent/browser,
      *                          or anything else), key to template mapping. Can be null.
      * @param thumbnail         thumbnail view of this type, can be null
      * @param permissionIds     list of permission ids, see {@link org.craftercms.studio.api.security.SecurityService}
+     * @param previewable       content type is previewable (like a page)
+     * @param lifecycleScripts  lifecycle method definitions to be invoked during the various
+     *                          lifecycle events, see {@link ??????????????????}
      * @param properties        key-value-pair properties, can be null
      * @return the content type descriptor
      * @throws org.craftercms.studio.commons.exception.StudioException
      */
     ContentType create(Context context, String site, String typeName, String formId, String defaultTemplateId,
-                       Map<String, String> templateIds, byte[] thumbnail, List<String> permissionIds, Map<String,
+                       Map<String, String> templateIds, byte[] thumbnail, List<String> permissionIds,
+                       boolean previewable, String lifecycleScripts, Map<String,
         String> properties) throws StudioException;
 
     // TODO consider simpler creation method as most of the params cannot be determined when a type is first created
-    // TODO missing label and previewable
     // TODO add lifecycle script placeholders
 
     /**
      * Update the form associated with a content type.
      *
-     * @param context           the caller's context
-     * @param site              the site to use
-     * @param contentTypeId     content type id
-     * @param formId            the form associated with this content type, see {@link org.craftercms.studio.api
-     * .content.FormService}
+     * @param context       the caller's context
+     * @param site          the site to use
+     * @param contentTypeId content type id
+     * @param formId        the form associated with this content type, see {@link org.craftercms.studio.api
+     *                      .content.FormService}
      * @return the content type descriptor
      * @throws StudioException
      */
@@ -73,49 +76,56 @@ public interface ContentTypeService {
     /**
      * Update the template map associated with a content type.
      *
-     * @param context           the caller's context
-     * @param site              the site to use
-     * @param contentTypeId     content type id
-     * @param templateIds       map of templates to use based on various criteria (like user-agent/browser,
-     *                          or anything else), key to template mapping. Can be null.
+     * @param context       the caller's context
+     * @param site          the site to use
+     * @param contentTypeId content type id
+     * @param templateIds   map of templates to use based on various criteria (like user-agent/browser,
+     *                      or anything else), key to template mapping. Can be null.
      * @return the content type descriptor
      * @throws StudioException
      */
-    ContentType updateTemplates(Context context, String site, String contentTypeId, List<String> templateIds) throws
-        StudioException;
+    ContentType updateTemplates(Context context, String site, String contentTypeId,
+                                List<String> templateIds) throws StudioException;
 
     /**
      * Update the thumbnail associated with a content type.
      *
-     * @param context           the caller's context
-     * @param site              the site to use
-     * @param contentTypeId     content type id
-     * @param thumbnail         thumbnail view of this type, can be null
+     * @param context       the caller's context
+     * @param site          the site to use
+     * @param contentTypeId content type id
+     * @param thumbnail     thumbnail view of this type, can be null
      * @return the content type descriptor
      * @throws StudioException
      */
-    ContentType updateThumbnail(Context context, String site, String contentTypeId, byte[] thumbnail) throws StudioException;
+    ContentType updateThumbnail(Context context, String site, String contentTypeId,
+                                byte[] thumbnail) throws StudioException;
 
     /**
      * Update the permissions associated with a content type.
      *
-     * @param context           the caller's context
-     * @param site              the site to use
-     * @param contentTypeId     content type id
-     * @param permissionIds     list of permission ids, see {@link org.craftercms.studio.api.security.SecurityService}
+     * @param context       the caller's context
+     * @param site          the site to use
+     * @param contentTypeId content type id
+     * @param permissionIds list of permission ids, see {@link org.craftercms.studio.api.security.SecurityService}
      * @return the content type descriptor
      * @throws StudioException
      */
-    ContentType updatePermissions(Context context, String site, String contentTypeId, List<String> permissionIds) throws
-        StudioException;
+    ContentType updatePermissions(Context context, String site, String contentTypeId,
+                                  List<String> permissionIds) throws StudioException;
 
+    // TODO document
+    ContentType updatePreviewable(Context context, String site, String contentTypeId,
+                                  boolean previewable) throws StudioException;
+    // TODO document
+    ContentType updateLifecycleScripts(Context context, String site, String contentTypeId,
+                                  String lifecycleScripts) throws StudioException;
     /**
      * Update the properties of a content type.
      *
-     * @param context           the caller's context
-     * @param site              the site to use
-     * @param contentTypeId     content type id
-     * @param properties        key-value-pair properties, can be null
+     * @param context       the caller's context
+     * @param site          the site to use
+     * @param contentTypeId content type id
+     * @param properties    key-value-pair properties, can be null
      * @return the content type descriptor
      * @throws StudioException
      */
@@ -125,10 +135,10 @@ public interface ContentTypeService {
     /**
      * Create a duplicate of a content type in the current site with a new name.
      *
-     * @param context           the caller's context
-     * @param site              the site to use
-     * @param contentTypeId     content type id
-     * @param typeName          name of content type
+     * @param context       the caller's context
+     * @param site          the site to use
+     * @param contentTypeId content type id
+     * @param typeName      name of content type
      * @return the content type descriptor
      * @throws StudioException
      */
@@ -137,11 +147,11 @@ public interface ContentTypeService {
     /**
      * Create a duplicate of a content type in a different site.
      *
-     * @param context           the caller's context
-     * @param sourceSite        the source site to copy from
-     * @param destinationSite   the destination site to copy to
-     * @param contentTypeId     the content type to copy
-     * @param typeName          name of the target content type
+     * @param context         the caller's context
+     * @param sourceSite      the source site to copy from
+     * @param destinationSite the destination site to copy to
+     * @param contentTypeId   the content type to copy
+     * @param typeName        name of the target content type
      * @return the content type descriptor
      * @throws StudioException
      */
@@ -151,9 +161,9 @@ public interface ContentTypeService {
     /**
      * Read a content type and return its descriptor.
      *
-     * @param context           the caller's context
-     * @param site              the site to use
-     * @param contentTypeId     content type id
+     * @param context       the caller's context
+     * @param site          the site to use
+     * @param contentTypeId content type id
      * @return the content type descriptor
      * @throws StudioException
      */
