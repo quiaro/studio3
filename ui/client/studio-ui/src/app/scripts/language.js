091ca3f30f6f54a709b42b12d03f2d3883790a49
quiaro <david.quiros@rivetlogic.com>
2014-05-21 18:36:08 -0600

    Uses preferences service inside the language service

--- ui/client/studio-ui/src/app/scripts/language.js
+++ ui/client/studio-ui/src/app/scripts/language.js
@@ -3,27 +3,39 @@
 
 angular.module('crafter.studio-ui.Language', [])
 
-    .factory('Language', ['$http', '$q', '$rootScope', '$state', '$log', 'Utils', 'GLOBALS',
-        function($http, $q, $rootScope, $state, $log, Utils, GLOBALS) {
+    .factory('Language', ['$http', '$q', '$rootScope', '$state', '$log', 'Utils', 'Preferences', 'GLOBALS',
+        function($http, $q, $rootScope, $state, $log, Utils, Preferences, GLOBALS) {
 
         // session storage language key
         var lang_key = 'studio_ui_lang';
 
-        if (localStorage) {
+        if (!Preferences.get(lang_key)) {
+            // If no language has been set, set it to the default
+            Preferences.set(lang_key, GLOBALS.default_language);
+        }
 
-            if (!localStorage[lang_key]) {
-                // If no language has been set, set it to the default
-                localStorage[lang_key] = GLOBALS.default_language;
-            }
+        return {
+            // Define where the language files will be read from
+            from: function from (baseUrl) {
+                var dfd = $q.defer(),
+                    fileName = Preferences.get(lang_key) + '.json',
+                    langFile = Utils.getUrl(baseUrl, fileName);
 
-            return {
-                // Define where the language files will be read from
-                from: function from (baseUrl) {
-                    var dfd = $q.defer(),
-                        fileName = localStorage[lang_key] + '.json',
-                        langFile = Utils.getUrl(baseUrl, fileName);
+                    $log.log('Loading language file: ', langFile);
 
-                        $log.log('Loading language file: ', langFile);
+                $http({
+                    method: 'GET',
+                    url: langFile,
+                    cache: true,
+                    responseType: 'json'
+                }).success(function(data) {
+                    dfd.resolve(data);
+                }).error(function() {
+                    $log.warn('Language file not found in: ' + langFile);
+                    $log.warn('Fallback to default language: ' + GLOBALS.default_language);
+
+                    fileName = GLOBALS.default_language + '.json';
+                    langFile = Utils.getUrl(baseUrl, fileName);
 
                     $http({
                         method: 'GET',
@@ -33,40 +45,22 @@ angular.module('crafter.studio-ui.Language', [])
                     }).success(function(data) {
                         dfd.resolve(data);
                     }).error(function() {
-                        $log.warn('Language file not found in: ' + langFile);
-                        $log.warn('Fallback to default language: ' + GLOBALS.default_language);
-
-                        fileName = GLOBALS.default_language + '.json';
-                        langFile = Utils.getUrl(baseUrl, fileName);
-
-                        $http({
-                            method: 'GET',
-                            url: langFile,
-                            cache: true,
-                            responseType: 'json'
-                        }).success(function(data) {
-                            dfd.resolve(data);
-                        }).error(function() {
-                            $log.error('Language file not found in: ' + langFile);
-                            dfd.reject('No appropriate language files found for module');
-                        });
+                        $log.error('Language file not found in: ' + langFile);
+                        dfd.reject('No appropriate language files found for module');
                     });
+                });
 
-                    return dfd.promise;
-                },
-                changeTo: function changeTo (langId) {
+                return dfd.promise;
+            },
+            changeTo: function changeTo (langId) {
 
-                    if (langId !== localStorage[lang_key]) {
-                        // Save the new language value in session storage and
-                        // and broadcast event to all listeners
-                        localStorage[lang_key] = langId;
-                        $rootScope.$broadcast('$sdoChangeLanguage', langId);
-                    }
+                if (langId !== Preferences.get(lang_key)) {
+                    // Save the new language value
+                    // and broadcast event to all listeners
+                    Preferences.set(lang_key, langId);
+                    $rootScope.$broadcast('$sdoChangeLanguage', langId);
                 }
-            };
-
-        } else {
-            throw new Error('Browser does not support Session Storage');
-        }
+            }
+        };
 
     }]);
